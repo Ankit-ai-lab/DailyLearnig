6c714ce3fa170f949d5051c5631262d51a11d2c3
Ankit Gupta <134631639+Ankit-ai-lab@users.noreply.github.com>
2025-09-04 00:25:21 +0530

    done

--- Coading/Auth JWT CRUD/index.js
+++ Coading/Auth JWT CRUD/index.js
@@ -1,14 +1,19 @@
-const express= require('express')
+/* const express= require('express')
+require('dotenv').config()
 const helmet = require('helmet');
 const cors = require('cors')
-
 const mongoose = require("mongoose")
-const cookieparser = require("cookie-parser") 
 
-const app  =express('require')
+
+const authRouter = require("./routers/authRouter")
+
+const app  =express()
 app.use(cors())
 app.use(helmet())
-app.use(cookieparser())
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 
@@ -23,9 +28,9 @@ mongoose.connect(process.env.MONGO_URI)
     console.log(err)
 })
 
-
+app.use('/auth',authRouter)
 app.get('/',(req,res)=>{
-    res.json({massage:"hello word from server.js"})
+    res.json({message:"hello word from server.js"})
 })
 
 
@@ -33,3 +38,23 @@ app.listen(process.env.PORT,()=>{
     console.log("listning..")
 });
 
+ */
+
+const express = require('express');
+const authRouter = require('./routers/authRouter');
+
+const app = express();
+app.use(express.json());
+
+// Mount router
+/* app.post('/signup', (req, res) => {
+    res.json({ message: 'Signup success' });
+}); */
+app.use('/auth', authRouter)
+
+// Test route
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello world' });
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
