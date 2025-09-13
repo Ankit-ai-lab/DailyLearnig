9956598da90d30101187fab08a29d9ea4dd009f3
Ankit Gupta <134631639+Ankit-ai-lab@users.noreply.github.com>
2025-09-13 08:58:20 +0530

    add

---
+++ Coading/Auth with todo class activitty/S11/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const connectToDB = require("./configs/mongodb.config");
+const UserRouter = require("./routes/user.routes");
+const TodoRouter = require("./routes/todo.routes");
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+connectToDB()
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  try {
+    res.status(200).json({ message: "This is test route" });
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+/// User Router
+app.use("/users", UserRouter)
+
+//// Todo Router 
+app.use("/todos", TodoRouter)
+// Handling undefined route
+app.use((req, res) => {
+  try {
+    res.status(200).json({ message: "This request is undefined" });
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server started");
+});
